cb87b0a5f5f38e3908f5a016fd7a94bccc43e1b1
luucamay <lupemaydana@gmail.com>
2019-12-03 00:01:48 -0400

    Verify that the dav file is not being processed or has been processed

--- scanea_videos.py
+++ scanea_videos.py
@@ -18,8 +18,16 @@ def main():
 	for arch in lista:
 		print("Procesando:", arch)
 		rutacompdav = os.path.join(sys.argv[2], arch)
+		arch_lock = str(arch) + ".lock"
+		arch_done = str(arch) + ".done"
+		# pending: update lista all the time
+		if arch_lock in lista or arch_done in lista:
+			continue
 		archsalida = str(arch) + ".log"		
 		fn("scanpatvid", sys.argv[1], rutacompdav, archsalida)
+		# save video in server video
+		# register the dav file processed
+		# move dav file to processed dir
 		time.sleep(2)
 
 if __name__ == "__main__":
