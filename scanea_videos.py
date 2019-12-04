a07ba4af6097856b6e22ee031dadec77bcc511a5
luucamay <lupemaydana@gmail.com>
2019-12-04 00:42:02 -0400

    Added done file, moved files to video server

--- scanea_videos.py
+++ scanea_videos.py
@@ -28,14 +28,20 @@ def main():
 			continue
 		
 		lock_file = open(arch_lock, 'w')
-		fcntl.lockf(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
+		fcntl.lockf(lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
 
 		archsalida = str(arch) + ".log"
 		fn("scanpatvid", sys.argv[1], rutacompdav, archsalida)
 		lock_file.close()
+		done_file = open(arch_done, 'w')
+		done_file.write("")
+		done_file.close()
+		# remove lock file
+		os.remove(lock_file)
 
 		shutil.move(rutacompdav, ruta_dav_procesado)
-		# register the dav file processed
+		shutil.move(arch_done, ruta_dav_procesado + ".done")
+		# register the dav file processed: archsalida ya tiene ese registro, es un archivo.log, en qué debería cambiar?
 
 		time.sleep(2)
 		lista = os.listdir(sys.argv[2])
