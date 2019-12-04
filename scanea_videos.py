75b608907a96cb7ea7684aefc91ce928fa26efc3
luucamay <lupemaydana@gmail.com>
2019-12-04 00:20:22 -0400

    Create the lock file

--- scanea_videos.py
+++ scanea_videos.py
@@ -4,6 +4,7 @@ import sys
 import subprocess
 import shutil
 import time
+import fcntl
 
 def fn(comando, dirpat, archdav, archsalida):
 	salida = subprocess.check_output([comando, dirpat, archdav])
@@ -13,26 +14,31 @@ def fn(comando, dirpat, archdav, archsalida):
 def main():
 	if len(sys.argv) != 3:
 		print("Modo de empleo:")
-		print(sys.argv[0], " DIR_PATRONES DIR_VIDEOS_DAV")
+		print(sys.argv[0], " DIR_PATRONES DIR_VIDEOS_DAV DIR_VIDEOS_PROCESADOS")
 		return
 	lista = os.listdir(sys.argv[2])
 	for arch in lista:
 		print("Procesando:", arch)
 		rutacompdav = os.path.join(sys.argv[2], arch)
-		# check if for this path, take name from argument or create one
-		ruta_dav_procesado = os.path.join(sys.argv[2], arch)
-		arch_lock = str(arch) + ".lock"
-		arch_done = str(arch) + ".done"
-		# pending: update lista all the time
+		ruta_dav_procesado = os.path.join(sys.argv[3], arch)
+				
+		arch_lock = rutacompdav + ".lock"
+		arch_done = rutacompdav + ".done"
 		if arch_lock in lista or arch_done in lista:
 			continue
-		archsalida = str(arch) + ".log"		
+		
+		lock_file = open(arch_lock, 'w')
+		fcntl.lockf(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
+
+		archsalida = str(arch) + ".log"
 		fn("scanpatvid", sys.argv[1], rutacompdav, archsalida)
-		# move dav file to processed dir
+		lock_file.close()
+
 		shutil.move(rutacompdav, ruta_dav_procesado)
 		# register the dav file processed
 
 		time.sleep(2)
+		lista = os.listdir(sys.argv[2])
 
 if __name__ == "__main__":
 	main()
