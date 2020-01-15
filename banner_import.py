056691f560b4e023fdf0b23dda8804f2d5a268b2
luucamay <lupemaydana@gmail.com>
2020-01-15 10:59:07 -0400

    Get data from log file

--- banner_import.py
+++ banner_import.py
@@ -2,6 +2,28 @@
 import os
 import sys
 
+def procesa_log(arch):
+    lista_datos = []
+    try:
+        with open(arch) as file:
+            for line in file:
+                line_array = line.split(' ')
+                line_set = set(line_array)
+                if 'MATCH' in line_set:
+                    log_data = {}
+                    for i in range(len(line_array)):
+                        if line_array[i] == 'MATCH':
+                            log_data['nombre_spot'] = line_array[i+1]
+                            log_data['duracion'] = line_array[i+2]
+                            log_data['cod_rubro'] = line_array[i+3]
+                            log_data['cod_producto'] = line_array[i+4]
+                            log_data['cod_anunciante'] = line_array[i+5]
+                    # linea procesada en log_data
+                    lista_datos.append(log_data)
+    except IOError:
+        print("No se puede acceder al archivo", arch)
+    return lista_datos
+
 def main():
     # process the string name
     if len(sys.argv) != 2:
@@ -17,13 +39,8 @@ def main():
         print("Procesando: ", arch)
         arch_dav = os.path.join(sys.argv[1], arch)
         arch_log = arch_dav + ".log"
-        try:
-            with open(arch_log) as f:
-                print("log file has been found")
-                # print(f.readlines())
-                # Do something with the file
-        except IOError:
-            print("File not accessible")
+        datos = procesa_log(arch_log)
+        print(datos)
 
     # process the text inside the log file
 
