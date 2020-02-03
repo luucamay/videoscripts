30308eadae1e213d0d882ffa6227ffac4e150257
luucamay <lupemaydana@gmail.com>
2020-02-03 10:26:40 -0400

    Modifyting get ciudad to take a parameter

--- banner_import_sql.py
+++ banner_import_sql.py
@@ -8,10 +8,20 @@ mydb = mysql.connector.connect(
 # TODO check if successful connection
 
 def get_ciudad(cod_ciu):
-    cod_ciu = cod_ciu.split('-')[0]
+    cod_ciu = cod_ciu.split('-')[1]
     ciudad = ''
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT nom_ciu FROM tciudad")
-    ciudad = mycursor.fetchone()[0]
+    sql = "SELECT nom_ciu FROM tciudad WHERE cod_ciu = %s"
+    val = (cod_ciu, )
+    mycursor.execute(sql, val)
+    result = mycursor.fetchone()[0]
+    if result:
+        ciudad = result
     return ciudad
 
+def get_canal(cod_canal):
+    canal = ''
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT nombre FROM tvcanal")
+    canal = mycursor.fetchone()[0]
+    return canal
