f8fc9bf61e8722595c235acf6cb4149639d87aaa
luucamay <lupemaydana@gmail.com>
2020-02-03 12:13:51 -0400

    Creating and inserting records for tables tvrubro, tvanunciante, tvproducto

--- database_setup.sql
+++ database_setup.sql
@@ -36,3 +36,38 @@ CREATE TABLE `tvcanal` (
 
 INSERT INTO tvcanal (cod_canal, nombre, numero, cod_ciudad)
 VALUES ('1', 'BTV', '5', '200');
+
+CREATE TABLE `tvrubro` (
+  `cod_rubro` int(11) NOT NULL,
+  `nom_rubro` varchar(255) NOT NULL,
+  `estado` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`cod_rubro`),
+  UNIQUE KEY `nom_rubro` (`nom_rubro`)
+);
+
+INSERT INTO tvrubro (cod_rubro, nom_rubro, estado)
+VALUES (159, 'BANCOS', 1);
+
+CREATE TABLE `tvanunciante` (
+  `cod_anu` int(11) NOT NULL,
+  `anunciante` varchar(255) NOT NULL,
+  `estado` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`cod_anu`),
+  UNIQUE KEY `anun` (`anunciante`)
+);
+
+INSERT INTO tvanunciante (cod_anu, anunciante, estado)
+VALUES (9, 'BANCO MERCANTIL SANTA CRUZ S.A.',1 );
+
+CREATE TABLE `tvproducto` (
+  `cod_producto` int(10) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `cod_anu` int(10) NOT NULL,
+  `cod_rubro` int(10) NOT NULL,
+  `estado` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`cod_producto`),
+  KEY `u_prodtv` (`cod_anu`,`nombre`)
+);
+
+INSERT INTO tvproducto (cod_producto, nombre, cod_anu, cod_rubro, estado)
+VALUES (1313, 'INSTITUCIONAL', 9, 159, 1);
